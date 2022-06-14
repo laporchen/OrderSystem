73d6ee7d72d93ecadd71c7ae7e7ecec7c8c0936b
creeperjong <leogood50203@gmail.com>
2022-06-14 19:26:18 +0800

    fix update of getOrderIdAsCart

--- sql/procedure.sql
+++ sql/procedure.sql
@@ -125,6 +125,10 @@ CREATE PROCEDURE getOrderIdAsCart (
                 FROM orders
                 WHERE cus_uname = orders.cus_uname;
             END;
+        ELSE
+            UPDATE orders
+            SET orders.total = total
+            WHERE order_id = orders.order_id;
         END IF;
         SELECT order_id;
     END //
